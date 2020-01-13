4af6ae0674018d7c32f590e606529b055d711bf1
Simone Celia <simon387@hotmail.it>
2020-01-13 18:22:34 +0100

    aadded_animations

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -13,12 +14,20 @@ const Contacts = () => {
 
 	return (
 		<Fragment>
+			<TransitionGroup>
 			{filtered !== null
-			? filtered.map(contact => (<ContactItem key={contact.id} contact={contact} />))
+			? filtered.map(contact => (
+				<CSSTransition key={contact.id} timeout={500} classNames="item">
+					<ContactItem contact={contact} />
+				</CSSTransition>
+				))
 			: contacts.map(contact => (
-				<ContactItem key={contact.id} contact={contact} />
+				<CSSTransition key={contact.id} timeout={500} classNames="item">
+					<ContactItem contact={contact} />
+				</CSSTransition>
 			))
 			}
+			</TransitionGroup>
 		</Fragment>
 	)
 }
