341174ea6186358abe6abf286be33f1e445ddbf5
Simone Celia <simon387@hotmail.it>
2020-01-28 15:15:34 +0100

    load_user_and_set_token

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
+const Home = () => {
+	const authContext = useContext(AuthContext);
 
-const Home = () => {//12.57
+	useEffect(() => {
+		authContext.loadUser();
+
+		// eslint-disable-disable-next-line
+	}, []);
+
 	return (
 		<div className="grid-2">
 			<div>
